2cac3b32fc4a96a595c0636ef056b5a728975362
huhuiwen <1050615501@qq.com>
2022-09-12 17:45:04 -0500

    add delete method

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,14 @@
+import { FaTimes} from 'react-icons/fa'
 import PropTypes from "prop-types"
 import Card from "../shared/Card"
-import React from "react"
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
     return (
         <Card reverse={false}>
             <div className="num-display">{item.rating}</div>
+            <button onClick={() => handleDelete(item.id)} className="close">
+                <FaTimes color='purple'/>
+            </button>
             <div className="text-display">{item.text}</div>
             {/* <button onClick={handleClick}>Click</button> */}
         </Card>
